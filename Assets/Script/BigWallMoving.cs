5b2f503160f6cc7d67f09da043d5daf3234caacc
Jingyo Kim <dosiresol@gmail.com>
2024-07-16 16:50:24 +0900

    alpha 조엉

---
+++ Assets/Script/BigWallMoving.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BigWallMoving : MonoBehaviour
+{
+    public enum WallType
+    {
+        PX,
+        NX,
+        PY,
+        NY,
+        PZ,
+        NZ,
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
